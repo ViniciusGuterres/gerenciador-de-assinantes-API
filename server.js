8ea779402cfc0e0207daac1f13be4cb89b2a2126
vinicius guterres <vinicius@spacecom.com.br>
2023-06-02 21:14:40 -0300

    Creating server and db

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoDB = require('./db/mongo.js');
 
 require('./routes/index.js')(app);
 
 app.use(cors());
 app.use(bodyParser.json());
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+
+    // Stating mongo  
+    mongoDB();
+});
