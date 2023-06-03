94fe75e378bd4784ec8417caa7f4c3842d7a7ec4
vinicius guterres <vinicius@spacecom.com.br>
2023-06-03 20:39:14 -0300

    Update SubscribersRoutes.js

--- routes/SubscribersRoutes.js
+++ routes/SubscribersRoutes.js
@@ -3,12 +3,12 @@ const bodyParser = require('body-parser');
 // Controllers
 const saveSubscriberController = require('../controllers/saveSubscriber.js').saveSubscriber;
 const deleteSubscriberController = require('../controllers/deleteSubscriber.js').deleteSubscriber;
-const getUsersController = require('../controllers/getSubscribers.js').getSubscribers;
+const getSubscribersController = require('../controllers/getSubscribers.js').getSubscribers;
 const updateSubscriberController = require('../controllers/updateSubscriber.js').updateSubscriber;
 
 module.exports = (app) => {
     app.post('/postSubscriber', bodyParser.json(), saveSubscriberController);
     app.delete('/deleteSubscriber/:id', deleteSubscriberController);
-    app.get('/getSubscribers/:id?', bodyParser.json(),getUsersController);
+    app.get('/getSubscribers/:id?', bodyParser.json(),getSubscribersController);
     app.put('/putSubscriber/:id', bodyParser.json(), updateSubscriberController);
 }
